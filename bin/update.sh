c65b7ea185f263621088c230703fe7c70ec2d589
Valerii Savchenko <valerii.s@giantleaplab.com>
2015-01-20 11:37:38 +0200

    Change script update

--- bin/update.sh
+++ bin/update.sh
@@ -2,4 +2,13 @@
 
 set -e
 
-cp ~/.gitconfig ../.gitconfig_sample
+SRC="$HOME/.gitconfig"
+DST="../.gitconfig_sample"
+
+echo cp "$SRC" "$DST"
+cp "$SRC" "$DST"
+sed 's/Valerii Savchenko.*$/your_name/g' -i "$DST"
+sed 's/valerii.s\@.*$/your@mail/g' -i "$DST"
+sed 's/wellic/yournik/g' -i "$DST"
+
+
