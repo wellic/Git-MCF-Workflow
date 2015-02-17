250d6c15796ff0db3c9caab224555a579c3a759e
Valerii Savchenko <valerii.s@giantleaplab.com>
2015-02-17 17:28:34 +0200

    Some fixes

--- tools/prepare_install.sh
+++ tools/prepare_install.sh
@@ -2,8 +2,14 @@
 
 set -e
 
-SRC_DIR=./before_install
-DST_DIR=./install
+cd "$(dirname "$0")"
+CURDIR=$PWD
+cd -
+clear
+SRC_DIR="$CURDIR"/before_install
+DST_DIR="$CURDIR"/install
+
+[ ! -f "$SRC_DIR"/my_setup_name.sh ] && ( cp "$SRC_DIR"/{01,my}_setup_name.sh; echo; echo "Please, prepare file '$SRC_DIR/my_setup_name.sh'"; exit 1 )
 
 mkdir -p "$DST_DIR/old"
 
@@ -12,5 +18,21 @@ mkdir -p "$DST_DIR/old"
 [ -f "$DST_DIR"/90_fix_win.sh    ] && mv "$DST_DIR"/{,old/}90_fix_win.sh
 
 cp {"$SRC_DIR","$DST_DIR"}/00_install.sh
-[ -f "$SRC_DIR"/my_setup_name.sh ] && cp "$SRC_DIR"/my_setup_name.sh "$DST_DIR"/01_setup_name.sh || cp {"$SRC_DIR","$DST_DIR"}/01_setup_name.sh
+[ -f "$SRC_DIR"/my_setup_name.sh ] && cp "$SRC_DIR"/my_setup_name.sh "$DST_DIR"/01_setup_name.sh || ( cp {"$SRC_DIR","$DST_DIR"}/01_setup_name.sh; "$DST_DIR"/{01,my}_setup_name.sh )
 cp {"$SRC_DIR","$DST_DIR"}/90_fix_win.bat
+
+echo
+echo "The directory 'install' was prepared"
+echo
+echo "Before install check file: '$DST_DIR/01_setup_name.sh'"
+echo
+echo "### Begin check file"
+echo "### $DST_DIR/01_setup_name.sh'"
+echo
+cat "$DST_DIR"/01_setup_name.sh
+echo
+echo "### End check file"
+echo
+echo "For start install:"
+echo "sh \"$DST_DIR/00_install_name.sh\""
+echo
