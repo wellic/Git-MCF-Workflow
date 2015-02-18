f0ffbf40f6f9f92817e8f6b4b2936a8285777827
Valerii Savchenko <valerii.s@giantleaplab.com>
2015-02-18 16:48:30 +0200

    Added clear install tools

--- install/00_prepare_install.sh
+++ install/00_prepare_install.sh
@@ -7,16 +7,21 @@ CUR_DIR=$PWD
 cd -
 clear
 SRC_DIR="$CUR_DIR"/install.template
-FNAME="my_git_name_info"
+SRC_FNAME="git_name_info"
+DST_FNAME=my_"$SRC_FNAME"
 
-[ ! -f "$CUR_DIR"/$FNAME ] && ( cp {"$SRC_DIR","$CUR_DIR"}/$FNAME; echo -e "\e[1;31m""\nPlease, prepare file '$CUR_DIR/$FNAME'\n""\e[0m"; exit 1 )
+print_check() {
+echo  -e "\e[1;36m""Check user info !!!\n""\e[0m"
+echo "Check info in the '$CUR_DIR/$DST_FNAME':"
+echo -e "\e[1;34m--------- START CHECK ---------\e[1;33m"
+cat "$CUR_DIR"/"$DST_FNAME"
+echo -e "\e[1;34m---------- END CHECK ----------\e[0m"
+echo -e "\nFor edit user info:"
+echo -e "\e[0;32m   vi \"$CUR_DIR/$DST_FNAME\"\e[0m"
+}
 
-echo  -e "\e[1;36m""\nPlease check user info !!!\n""\e[0m"
-echo "Check info in the '$CUR_DIR/$FNAME':"
-echo -e "\e[1;34m""--------- START CHECK ---------\n""\e[1;33m"
-cat "$CUR_DIR"/"$FNAME"
-echo -e "\e[1;34m""\n---------- END CHECK ----------\n""\e[0m"
-echo "For edit user info:"
-echo  -e "\e[0;32m""vi \"$CUR_DIR/$FNAME\"""\e[0m"
+[ ! -f "$CUR_DIR"/"$DST_FNAME" ] && ( cp "$SRC_DIR"/"$SRC_FNAME" "$CUR_DIR"/"$DST_FNAME"; print_check; echo -e "\e[1;31m""\nPlease, edit file '$CUR_DIR/$DST_FNAME'""\e[0m"; exit 1 )
+
+print_check
 
 exit 0
--- install/install.template/00_install.sh
+++ install/install.template/00_install.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-mv ~/.gitconfig ~/.gitconfig.$(date +%Y%m%d_%H%M%S)
-cp ../../.gitconfig_sample ~/.gitconfig
+NAME_CFG=".gitconfig"
+NAME_TMPL=".gitconfig_sample"
+
+cp -v ~/"$NAME_CFG" ~/"${NAME_CFG}.$(date +%Y%m%d_%H%M%S).old"
+
+CUR_DIR=$(dirname "$0")
+cd "$CUR_DIR"/../../
+CFG_DIR=$PWD
+cd -
+
+[ ! -f "$CFG_DIR"/"$NAME_TMPL" ] && (echo -e "\e[1;31m\nSome error. Check exists script '$CFG_DIR/$NAME_TMPL'\e[0m"; exit 1 )
+
+cp -v "$CFG_DIR"/"$NAME_TMPL" ~/"$NAME_CFG"
 
