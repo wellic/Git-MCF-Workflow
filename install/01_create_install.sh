9137423c264d33d3afb7300c60392c2057a7e695
Valerii Savchenko <valerii.s@giantleaplab.com>
2015-02-18 18:08:41 +0200

    Improved output info

--- install/01_create_install.sh
+++ install/01_create_install.sh
@@ -5,6 +5,8 @@ set -e
 cd "$(dirname "$0")"
 CUR_DIR=$PWD
 cd -
+clear
+
 SRC_DIR="$CUR_DIR"/install.template
 NAME_INSTALL_DIR=_install
 NAME_INSTALL_PRG=00_install.sh
@@ -13,13 +15,10 @@ DST_DIR="$CUR_DIR"/"$NAME_INSTALL_DIR"
 FILE_NAME_INFO="my_git_name_info"
 
 bash "$CUR_DIR/00_prepare_install.sh"
-
 [ $? ] || exit 1
 
-mkdir -p "$DST_DIR/old"
-
-[ -f "$DST_DIR"/"$NAME_INSTALL_PRG" ] && mv "$DST_DIR"/{,old/}"$NAME_INSTALL_PRG"
-[ -f "$DST_DIR"/"$NAME_FIX_PRG"     ] && mv "$DST_DIR"/{,old/}"$NAME_FIX_PRG"
+[ -d  ] && rm -rf "$DST_DIR"
+mkdir -p "$DST_DIR"
 
 cp {"$SRC_DIR","$DST_DIR"}/"$NAME_INSTALL_PRG"
 echo >> "$DST_DIR"/"$NAME_INSTALL_PRG"
@@ -27,10 +26,10 @@ echo "#user info" >> "$DST_DIR"/"$NAME_INSTALL_PRG"
 cat "$CUR_DIR"/"$FILE_NAME_INFO" >> "$DST_DIR"/"$NAME_INSTALL_PRG"
 cp {"$SRC_DIR","$DST_DIR"}/"$NAME_FIX_PRG"
 
-echo -e "\e[1;36m""\nThe install program was prepared.\n""\e[0m"
-echo "1. All user have to run installation after checked user info correct:"
-echo  -e "\e[0;32m   bash \"$DST_DIR/$NAME_INSTALL_PRG\"\n\e[0m"
-echo "2. After installation Windows users have to start the additional fix."
+echo -e "\e[1;36mThe install program was prepared.\e[0m"
+echo -e "\n1. All user have to run installation after checked user info correct:"
+echo -e "\e[0;32m   bash \"$DST_DIR/$NAME_INSTALL_PRG\"\e[0m"
+echo -e "\n2. After installation Windows users have to start the additional fix."
 echo -e "   Go to the directory \e[0;32m'${NAME_INSTALL_DIR}'\e[0m with file \e[0;32m'${NAME_FIX_PRG}'\e[0m and start it.\n"
 
 exit 0
