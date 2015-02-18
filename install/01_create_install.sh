088dfd32a6c128768852e2dc7e2ca9056df6e123
Valerii Savchenko <valerii.s@giantleaplab.com>
2015-02-18 16:48:30 +0200

    Added clear install tools

--- install/01_create_install.sh
+++ install/01_create_install.sh
@@ -6,25 +6,30 @@ cd "$(dirname "$0")"
 CUR_DIR=$PWD
 cd -
 SRC_DIR="$CUR_DIR"/install.template
-DST_DIR="$CUR_DIR"/_install
-FNAME="my_git_name_info"
+NAME_INSTALL_DIR=_install
+NAME_INSTALL_PRG=00_install.sh
+NAME_FIX_PRG=90_fix_win.bat
+DST_DIR="$CUR_DIR"/"$NAME_INSTALL_DIR"
+FILE_NAME_INFO="my_git_name_info"
 
-"$CUR_DIR"/00_prepare_install.sh
+sh -c "\"$CUR_DIR/00_prepare_install.sh\""
+[ $? ] || exit 1
 
 mkdir -p "$DST_DIR/old"
 
-[ -f "$DST_DIR"/00_install.sh    ] && mv "$DST_DIR"/{,old/}00_install.sh
-[ -f "$DST_DIR"/90_fix_win.sh    ] && mv "$DST_DIR"/{,old/}90_fix_win.sh
+[ -f "$DST_DIR"/"$NAME_INSTALL_PRG" ] && mv "$DST_DIR"/{,old/}"$NAME_INSTALL_PRG"
+[ -f "$DST_DIR"/"$NAME_FIX_PRG"     ] && mv "$DST_DIR"/{,old/}"$NAME_FIX_PRG"
 
-cp {"$SRC_DIR","$DST_DIR"}/00_install.sh
-echo >> "$DST_DIR"/00_install.sh
-echo "#user info" >> "$DST_DIR"/00_install.sh
-cat "$CUR_DIR"/"$FNAME" >> "$DST_DIR"/00_install.sh
-cp {"$SRC_DIR","$DST_DIR"}/90_fix_win.bat
+cp {"$SRC_DIR","$DST_DIR"}/"$NAME_INSTALL_PRG"
+echo >> "$DST_DIR"/"$NAME_INSTALL_PRG"
+echo "#user info" >> "$DST_DIR"/"$NAME_INSTALL_PRG"
+cat "$CUR_DIR"/"$FILE_NAME_INFO" >> "$DST_DIR"/"$NAME_INSTALL_PRG"
+cp {"$SRC_DIR","$DST_DIR"}/"$NAME_FIX_PRG"
 
 echo -e "\e[1;36m""\nThe install program was prepared.\n""\e[0m"
 echo "1. All user have to run installation after checked user info correct:"
-echo  -e "\e[0;32m""sh \"$DST_DIR/00_install_name.sh\"\n""\e[0m"
+echo  -e "\e[0;32m   sh -c \"$DST_DIR/$NAME_INSTALL_PRG\"\n\e[0m"
 echo "2. After installation Windows users have to start the additional fix."
-echo "Go to the directory '$DST_DIR' and start fix:"
-echo  -e "\e[0;32m""90_fix_win.bat\n""\e[0m"
+echo -e "   Go to the directory \e[0;32m'${NAME_INSTALL_DIR}'\e[0m with file \e[0;32m'${NAME_FIX_PRG}'\e[0m and start it.\n"
+
+exit 0
---
+++ install/99_clear.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+CUR_DIR=$PWD
+cd -
+
+SRC_DIR="$CUR_DIR"/install.template
+NAME_INSTALL_DIR=_install
+NAME_INSTALL_PRG=00_install.sh
+NAME_FIX_PRG=90_fix_win.bat
+DST_DIR="$CUR_DIR"/"$NAME_INSTALL_DIR"
+FILE_NAME_INFO="my_git_name_info"
+
+[ -d "$CUR_DIR"/"$NAME_INSTALL_DIR" ] &&  rm -rf "$CUR_DIR"/"$NAME_INSTALL_DIR"
+[ -f "$CUR_DIR"/"$FILE_NAME_INFO" ] &&  mv -v "$CUR_DIR"/"$FILE_NAME_INFO" "$CUR_DIR"/"${FILE_NAME_INFO}.$(date +%Y%m%d_%H%M%S).old"
+
+exit 0
