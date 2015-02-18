5d0ece3fe417fdd8557560852261a85132300782
Valerii Savchenko <valerii.s@giantleaplab.com>
2015-02-18 14:36:55 +0200

    Some fixes

--- install/install.template/00_install.sh
+++ install/install.template/00_install.sh
@@ -3,4 +3,3 @@
 mv ~/.gitconfig ~/.gitconfig.$(date +%Y%m%d_%H%M%S)
 cp ../../.gitconfig_sample ~/.gitconfig
 
-./01_setup_name.sh
--- tools/before_install/01_setup_name.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-git config --global user.name "your_name"
-git config --global user.email "your_email@your_email"
-git config --global user.nickname "your nick name"
-
