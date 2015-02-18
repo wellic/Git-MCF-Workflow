83ce89db910de1c328eea4b94f86e58e7e408d21
Valerii Savchenko <valerii.s@giantleaplab.com>
2015-02-19 00:07:49 +0200

    Fixed issue in installer

--- bin/save_local_conf.sh
+++ bin/save_local_conf.sh
@@ -2,9 +2,15 @@
 
 set -e
 
+RET_DIR=$PWD
 CUR_DIR=$(dirname "$0")
+
+cd "$CUR_DIR"/../
+MAIN_DIR=$PWD
+cd "$RET_DIR"
+
 DST_DIR="$CUR_DIR"/local_git_config
-GIT_DIR="$CUR_DIR"/../.git
+GIT_DIR="$MAIN_DIR"/.git
 
 #mkdir -p "$DST_DIR" 
 echo "cp $GIT_DIR/config $DST_DIR/config"
--- install/00_prepare_install.sh
+++ install/00_prepare_install.sh
@@ -1,27 +1,41 @@
 #!/bin/bash
 
+#You do not have to change the code if you do not understand that you do
+#Вы не должны изменять код, если вы не понимаете, что вы делаете.
+
 set -e
 
-cd "$(dirname "$0")"
+RET_DIR=$PWD
+CUR_DIR=$(dirname "$0")
+cd "$CUR_DIR"
 CUR_DIR=$PWD
-cd -
+cd "$RET_DIR"
 clear
-SRC_DIR="$CUR_DIR"/install.template
+
+DIR_INSTALL=_install
+FNAME_INSTALL=install.sh
+FNAME_FIX_WIN=fix_win.bat
+
 SRC_FNAME="git_name_info"
 DST_FNAME=my_"$SRC_FNAME"
 
+SRC_DIR="$CUR_DIR"/install.template
+
 print_check() {
-echo -e "\e[1;36mCheck user info ...\e[0m"
+echo -e "\033[1;36mCheck user info ...\033[0m"
 echo -e "\nCheck the file '$CUR_DIR/$DST_FNAME':"
-echo -e "\e[1;34m--------- START CHECK ---------\e[1;33m"
+echo -e "\033[1;34m--------- START CHECK ---------\033[1;33m"
 cat "$CUR_DIR"/"$DST_FNAME"
-echo -e "\e[1;34m---------- END CHECK ----------\e[0m"
+echo -e "\033[1;34m---------- END CHECK ----------\033[0m"
 echo -e "\nEdit user info:"
-echo -e "\e[0;32m   vi \"$CUR_DIR/$DST_FNAME\"\n\e[0m"
+echo -e "\033[0;32m   vi \"$CUR_DIR/$DST_FNAME\"\033[0m"
 }
 
-[ ! -f "$CUR_DIR"/"$DST_FNAME" ] && ( cp "$SRC_DIR"/"$SRC_FNAME" "$CUR_DIR"/"$DST_FNAME"; print_check; echo -e "\e[1;31mPlease, edit the file '$CUR_DIR/$DST_FNAME'\e[0m"; exit 1 )
+echo -e "\033[7;35mStart preparing install\n\033[0m"
 
+[ ! -f "$CUR_DIR"/"$DST_FNAME" ] && ( cp "$SRC_DIR"/"$SRC_FNAME" "$CUR_DIR"/"$DST_FNAME"; print_check; echo -e "\033[1;31mPlease, edit the file '$CUR_DIR/$DST_FNAME'\033[0m"; exit 1 )
 print_check
+
+echo -e "\033[7;35m\nFinish preparing install\033[0m"
 
 exit 0
