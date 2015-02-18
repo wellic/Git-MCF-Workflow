213ac46117f18fb38cc6fe3b7f0260782a3b39f2
Valerii Savchenko <valerii.s@giantleaplab.com>
2015-02-18 18:08:41 +0200

    Improved output info

--- install/00_prepare_install.sh
+++ install/00_prepare_install.sh
@@ -11,16 +11,16 @@ SRC_FNAME="git_name_info"
 DST_FNAME=my_"$SRC_FNAME"
 
 print_check() {
-echo  -e "\e[1;36m""Check user info !!!\n""\e[0m"
-echo "Check info in the '$CUR_DIR/$DST_FNAME':"
+echo -e "\e[1;36mCheck user info ...\e[0m"
+echo -e "\nCheck the file '$CUR_DIR/$DST_FNAME':"
 echo -e "\e[1;34m--------- START CHECK ---------\e[1;33m"
 cat "$CUR_DIR"/"$DST_FNAME"
 echo -e "\e[1;34m---------- END CHECK ----------\e[0m"
-echo -e "\nFor edit user info:"
-echo -e "\e[0;32m   vi \"$CUR_DIR/$DST_FNAME\"\e[0m"
+echo -e "\nEdit user info:"
+echo -e "\e[0;32m   vi \"$CUR_DIR/$DST_FNAME\"\n\e[0m"
 }
 
-[ ! -f "$CUR_DIR"/"$DST_FNAME" ] && ( cp "$SRC_DIR"/"$SRC_FNAME" "$CUR_DIR"/"$DST_FNAME"; print_check; echo -e "\e[1;31m""\nPlease, edit file '$CUR_DIR/$DST_FNAME'""\e[0m"; exit 1 )
+[ ! -f "$CUR_DIR"/"$DST_FNAME" ] && ( cp "$SRC_DIR"/"$SRC_FNAME" "$CUR_DIR"/"$DST_FNAME"; print_check; echo -e "\e[1;31mPlease, edit the file '$CUR_DIR/$DST_FNAME'\e[0m"; exit 1 )
 
 print_check
 
--- install/99_clear.sh
+++ install/99_clear.sh
@@ -5,6 +5,7 @@ set -e
 cd "$(dirname "$0")"
 CUR_DIR=$PWD
 cd -
+clear 
 
 SRC_DIR="$CUR_DIR"/install.template
 NAME_INSTALL_DIR=_install
@@ -13,7 +14,12 @@ NAME_FIX_PRG=90_fix_win.bat
 DST_DIR="$CUR_DIR"/"$NAME_INSTALL_DIR"
 FILE_NAME_INFO="my_git_name_info"
 
+
+echo -e "\e[1;36mClearing ...\e[0m"
+
 [ -d "$CUR_DIR"/"$NAME_INSTALL_DIR" ] &&  rm -vrf "$CUR_DIR"/"$NAME_INSTALL_DIR"
 #[ -f "$CUR_DIR"/"$FILE_NAME_INFO" ] &&  mv -v "$CUR_DIR"/"$FILE_NAME_INFO" "$CUR_DIR"/"${FILE_NAME_INFO}.$(date +%Y%m%d_%H%M%S).old"
+
+echo -e "\e[1;36mCleared\e[0m"
 
 exit 0
